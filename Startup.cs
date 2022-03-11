e572dfd1ed8b69e7c6a2785b15a8040a0ba0f9e4
MrOOI <ogabekochilov25@gmail.com>
2022-03-11 14:35:06 -0800

    startup add constring

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace EBazar
         {
             services.AddControllersWithViews();
             services.AddMvc();
-            services.AddDbContext<ProductContext>(options => options.UseNpgsql(Configuration.GetConnectionString("")));
+            services.AddDbContext<ProductContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConString")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
